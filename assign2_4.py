33c25924c3a6cb32e1843591d248d33b01596ab7
ymtowya <kaigendo@126.com>
2023-05-21 18:08:59 -0800

    Add inputs

--- assign2_4.py
+++ assign2_4.py
@@ -1,8 +1,9 @@
 
 my_array = []
 my_thres = 0
-my_expected = 0
+# my_expected = 0
 
+# Set the array of numbers by the reading input
 def set_arr(line):
     my_array = []
     numbers_str = line[1:-2]
@@ -11,6 +12,7 @@ def set_arr(line):
         my_array.append(int(num))
     return my_array
 
+# Set the threshold
 def set_thres(line):
     my_thres = float(line)
     return my_thres
@@ -19,6 +21,7 @@ def set_expected(line):
     my_expected = int(line)
     return my_expected
 
+# Get the critical number
 def find_critical_number(arr, threshold):
     count = 0
     n = len(arr)
@@ -28,22 +31,20 @@ def find_critical_number(arr, threshold):
                 count += 1
     return count
 
+# Do the computation and print out the result
 def do_compute_then_compare(my_array, my_thres):
     res = find_critical_number(my_array, my_thres)
     print("The array is: ", my_array)
     print("The threshold is: ", my_thres)
-    print("The expected is: ", my_expected)
     print("The actual result is: ", res)
     print("\n")
 
-with open("Module2HWTestCases.txt", "r") as file:
+with open("example_input.txt", "r") as file:
     line = file.readline()
     while not (line is None) and line != "":
         if line[0] == '[':
             my_array = set_arr(line)
             line = file.readline()
             my_thres = set_thres(line)
-            line = file.readline()
-            my_expected = set_expected(line)
             do_compute_then_compare(my_array, my_thres)
         line = file.readline()
