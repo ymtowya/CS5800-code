67a19a8831c3cb6956c7a0733146bfdcdb96e422
ymtowya <kaigendo@126.com>
2023-07-09 22:35:19 -0400

    update 2

--- assign8/as8_2.py
+++ assign8/as8_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 def find_coin_num(coins, S):
-    # sort the coins by value
-    coins.sort()
     n = len(coins)
     INF = 99999999
     # init the dp array with max value
@@ -15,13 +13,10 @@ def find_coin_num(coins, S):
         # traverse each type of coin
         for i in range(n):
             coin = coins[i]
-            # we add add this coin to the solution
+            # if we can add this coin to the solution
             if m >= coin:
                 # update the dp with minimum number of coins to reach this value
                 dp[m] = min(dp[m - coin] + 1, dp[m])
-            else:
-                # we don't need to consider larger coins
-                break
     
     if dp[S] == INF:
         return -1
@@ -29,7 +24,7 @@ def find_coin_num(coins, S):
 
 if __name__ == '__main__':
     S = 13
-    my_arr = [1, 3, 5, 7, 9]
+    my_arr = [3, 7, 9, 1, 5]
     print("Coins array : ", my_arr)
     print("Target value : ", S)
     print(find_coin_num(my_arr, S))
